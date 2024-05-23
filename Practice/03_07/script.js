24464e0b8bc8386d61d2b0e1dcc8a0afa0a5e583
Emma Vonbuelow <vonbuelow.e@northeastern.edu>
2024-05-23 15:16:51 -0500

    first practice activity

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,29 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const myPhone = {
+    name: "iPhone 10",
+    case: "WildFlower",
+    color: "Black",
+    gb: 128, 
+    charging: false, 
+    battery: 76,
+    toggleCharging: function (chargingStatus) {
+        this.charging = chargingStatus;
+        console.log(`Charging status changed.`);
+    },
+};
+
+const backpack = {
+    mainPocket: {
+        items: ["book", "sticky notes", "camera"],
+        open:  false,
+    },
+    pockets: 10,
+    color: "grey",
+    full: false,
+};
+
+console.log(myPhone);
+console.log(backpack);
